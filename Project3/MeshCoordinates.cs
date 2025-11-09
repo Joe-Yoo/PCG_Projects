a54b6f72d2f90d4768096eb4ffca7110f996a715
Joe <josephyoo50@gmail.com>
2025-11-09 00:49:15 -0500

    outline of pig with WINGS done

--- Project3/MeshCoordinates.cs
+++ Project3/MeshCoordinates.cs
@@ -5,28 +5,15 @@ using UnityEngine;
 
 public class MeshCoordinates
 {
-    public static List<(int, int, int)> test_0 = new List<(int, int, int)>
-    {
-        (0,0,0), (1,0,0), (0,0,1), (1,0,1), (0,0,2), (1,0,2),
-        (0,1,0), (1,1,0), (0,1,1), (1,1,1), (0,1,2), (1,1,2),
-    };
-
-    public static List<(int, int, int)> Test_1()
+    public static List<(int, int, int)> Body()
     {
         List<(int, int, int)> result = new List<(int, int, int)>();
 
         for (int x = 0; x < 12; x++)
             for (int y = 0; y < 7; y++)
                 for (int z = 0; z < 7; z++)
-                {
                     if (!(y == 6 && (z == 0 || z == 6)))
                         result.Add((x, y, z));
-                }
-
-        result.Add((12, 4, 3));
-        result.Add((13, 4, 3));
-        result.Add((13, 3, 3));
-        result.Add((13, 2, 3));
 
         for (int y = -1; y >= -2; y--)
         {
@@ -39,12 +26,74 @@ public class MeshCoordinates
         for (int x = -4; x < 1; x++)
             for (int y = 4; y < 9; y++)
                 for (int z = 1; z < 6; z++)
-                {
                     if (!result.Contains((x,y,z)))
                         result.Add((x, y, z));
-                }
-
 
         return result;
+    }
+
+    public static List<(int, int, int)> Wings() {
+        List<(int, int, int)> result = new List<(int, int, int)>();
+
+        for (int i = 0; i <= 3; i++) {
+            result.Add((-1,i,0));
+        }
+
+        for (int y = 0; y < 6; y++)
+            for (int x = 0; x <= y; x++) 
+                if (y - x <= 4)
+                    result.Add((x,y,0));
+            
+        return result;
+    }
+
+    public static List<(int, int, int)> Nostril_1() {
+        List<(int, int, int)> result = new List<(int, int, int)>();
+
+        for (int x = 0; x < 2; x++)
+            for (int y = 0; y < 2; y++)
+                for (int z = 0; z < 3; z++)
+                    result.Add((x,y,z));
+            
+        return result;
+    } 
+
+    public static List<(int, int, int)> Eyes() {
+        List<(int, int, int)> result = new List<(int, int, int)>();
+
+        result.Add((0,0,0));
+        result.Add((0,0,2));
+            
+        return result;
+    }
+
+    public static List<(int, int, int)> Tail_1() {
+        List<(int, int, int)> result = new List<(int, int, int)>();
+
+        result.Add((0, 3, 0));
+        result.Add((1, 3, 0));
+        result.Add((1, 2, 0));
+        result.Add((2, 2, 0));
+        result.Add((2, 1, 0));
+        result.Add((2, 0, 0));
+            
+        return result;
+    }
+
+    public static List<(int, int, int)> Tail_2() {
+        List<(int, int, int)> result = new List<(int, int, int)>();
+
+        result.Add((0, 3, 0));
+        result.Add((1, 3, 0));
+        result.Add((1, 2, 0));
+        result.Add((2, 2, 0));
+        result.Add((2, 1, 0));
+        result.Add((2, 0, 0));
+            
+        return result;
+    }
+
+    public static List<(int, int, int)> Single() {
+        return new List<(int, int, int)>() { (0, 0, 0) };
     }
 }
--- Project3/MeshCreator.cs
+++ Project3/MeshCreator.cs
@@ -12,100 +12,70 @@ public class Cube : MonoBehaviour {
 	private float u = 1f;
 	void Start() {
 		Random.InitState(seed);
-		(List<Quad>, List<Edge>) mesh_ = Create(Test_1());
-		
-		List<Quad> subdivided = CatmullClark(mesh_.Item1, mesh_.Item2);
-		Mesh mesh = CreateMesh(subdivided);
+		GameObject creature = new GameObject("Pig_With_Wings");
 
-		// Mesh mesh = CreateMesh(mesh_.Item1);
+		// ====== Textures ======
+		Material light_pink = CreateMat(new Color(0.99215686f, 0.84313726f, 0.89411765f, 1f));
+		Material dark_pink = CreateMat(new Color(0.99215686f, 0.70313726f, 0.70411765f, 1f));
+		Material white = CreateMat(new Color(1f, 1f, 1f, 1f));
+		Material black = CreateMat(new Color(0f, 0f, 0f, 1f));
 
-		GameObject m = new GameObject("Cube_");
-		m.AddComponent<MeshFilter>();
-		m.AddComponent<MeshRenderer>();
-		m.GetComponent<MeshFilter>().mesh = mesh;
-		m.transform.position = new Vector3(0, 0, 0);
-		Renderer rend_ = m.GetComponent<Renderer>();
-		Color c = new Color(Random.value, Random.value, Random.value, 1f);
+		// ======= Body =======
+		GameObject body = CreateGameObject("Body", Body(), creature, light_pink, 0, 0, 0);
+
+		// ======= Wings =======
+		GameObject left_wing = CreateGameObject("Left_Wing", Wings(), creature, white, 6, 5, 0);
+		GameObject right_wing = CreateGameObject("Right_Wing", Wings(), creature, white, 6, 5, 6);
+
+		// ======= Nose =======
+		GameObject nostril = CreateGameObject("Nostril", Nostril_1(), creature, dark_pink, -5, 5, 2);
+
+		// ====== Eyes ======
+		GameObject eyes = CreateGameObject("Eyes", Eyes(), creature, white, -4.5f, 7, 2);
+
+		GameObject left_pupil = CreateGameObject("Left_Pupil", Single(), eyes, black, -4.75f, 7, 2);
+		GameObject left_pivot = new GameObject("Left_Pivot");
+		left_pivot.transform.position = left_pupil.transform.position + new Vector3(u / 2, u / 2, u / 2);
+		left_pupil.transform.parent = left_pivot.transform;
+		left_pivot.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+		left_pivot.transform.parent = creature.transform;
+
+		GameObject right_pupil = CreateGameObject("Right_Pupil", Single(), eyes, black, -4.75f, 7, 4);
+		GameObject right_pivot = new GameObject("Right_Pivot");
+		right_pivot.transform.position = right_pupil.transform.position + new Vector3(u / 2, u / 2, u / 2);
+		right_pupil.transform.parent = right_pivot.transform;
+		right_pivot.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+		right_pivot.transform.parent = creature.transform;
+
+		// ====== Tail ======
+		GameObject tail = CreateGameObject("Tail", Tail_1(), creature, light_pink, 11.5f, 1.5f, 3);
+	}
+
+	GameObject CreateGameObject(string name, List<(int, int, int)> co, GameObject parent, Material mat, float x, float y, float z) {
+		(List<Quad>, List<Edge>) mesh_qe = Create(co);
+		List<Quad> subdiv_mesh = CatmullClark(mesh_qe.Item1, mesh_qe.Item2);
+
+		GameObject go = new GameObject(name);
+		go.AddComponent<MeshFilter>();
+		go.AddComponent<MeshRenderer>();
+		go.GetComponent<MeshFilter>().mesh = CreateMesh(subdiv_mesh);
+		go.transform.position = ScaleVector(x, y, z);
+		go.transform.parent = parent.transform;
+
+		Renderer rend = go.GetComponent<Renderer>();
+		rend.material = mat;
+		return go;
+	}
+
+	Material CreateMat(Color c) {
 		Material mat = new Material(Shader.Find("Standard"));
 		mat.color = c;
 		mat.SetInt("_CullMode", (int)CullMode.Off);
-		rend_.material = mat;
+		return mat;
 	}
 
-	(List<Quad>, List<Edge>) CreateCube() {
-
-		List<Quad> quads = new List<Quad>();
-		List<Edge> edges = new List<Edge>();
-
-		Vector3[] verts_ = new Vector3[24];
-
-		verts_[0] = new Vector3 ( 1, -1, -1);
-		verts_[1] = new Vector3 ( 1, -1,  1);
-		verts_[2] = new Vector3 (-1, -1,  1);
-		verts_[3] = new Vector3 (-1, -1, -1);
-
-		verts_[4] = new Vector3 (-1,  1, -1);
-		verts_[5] = new Vector3 (-1,  1,  1);
-		verts_[6] = new Vector3 ( 1,  1,  1);
-		verts_[7] = new Vector3 ( 1,  1, -1);
-
-		verts_[8]  = new Vector3 (-1,  1,  1);
-		verts_[9]  = new Vector3 (-1,  1, -1);
-		verts_[10] = new Vector3 (-1, -1, -1);
-		verts_[11] = new Vector3 (-1, -1,  1);
-
-		verts_[12] = new Vector3 ( 1,  1,  1);
-		verts_[13] = new Vector3 (-1,  1,  1);
-		verts_[14] = new Vector3 (-1, -1,  1);
-		verts_[15] = new Vector3 ( 1, -1,  1);
-
-		verts_[16] = new Vector3 ( 1,  1, -1);
-		verts_[17] = new Vector3 ( 1,  1,  1);
-		verts_[18] = new Vector3 ( 1, -1,  1);
-		verts_[19] = new Vector3 ( 1, -1, -1);
-
-		verts_[20] = new Vector3 (-1,  1, -1);
-		verts_[21] = new Vector3 ( 1,  1, -1);
-		verts_[22] = new Vector3 ( 1, -1, -1);
-		verts_[23] = new Vector3 (-1, -1, -1);
-
-		Quad q0 = new Quad(verts_[0], verts_[1], verts_[2], verts_[3]);
-		Quad q1 = new Quad(verts_[4], verts_[5], verts_[6], verts_[7]);
-		Quad q2 = new Quad(verts_[8], verts_[9], verts_[10], verts_[11]);
-		Quad q3 = new Quad(verts_[12], verts_[13], verts_[14], verts_[15]);
-		Quad q4 = new Quad(verts_[16], verts_[17], verts_[18], verts_[19]);
-		Quad q5 = new Quad(verts_[20], verts_[21], verts_[22], verts_[23]);
-
-		quads.Add(q0);
-		quads.Add(q1);
-		quads.Add(q2);
-		quads.Add(q3);
-		quads.Add(q4);
-		quads.Add(q5);
-
-		for (int i = 0; i < quads.Count; i++)
-		{
-			for (int j = i + 1; j < quads.Count; j++)
-			{
-				List<Vector3> shared = new List<Vector3>();
-				Vector3[] a = new Vector3[] { quads[i].v1, quads[i].v2, quads[i].v3, quads[i].v4 };
-				Vector3[] b = new Vector3[] { quads[j].v1, quads[j].v2, quads[j].v3, quads[j].v4 };
-				foreach (Vector3 av in a)
-				{
-					foreach (Vector3 bv in b)
-					{
-						if (av == bv && !shared.Contains(av)) shared.Add(av);
-					}
-				}
-				if (shared.Count == 2)
-				{
-					Edge edge = new Edge(shared[0], shared[1], quads[i], quads[j]);
-					edges.Add(edge);
-				}
-			}
-		}
-
-		return (quads, edges);
+	Vector3 ScaleVector(float x, float y, float z) {
+		return new Vector3(x * u, y * u, z * u);
 	}
 
 	Mesh CreateMesh(List<Quad> quads)
@@ -238,7 +208,7 @@ public class Cube : MonoBehaviour {
 		{
 			var key = (vx, vy, vz);
 			if (pool.TryGetValue(key, out Vector3 v)) return v;
-			Vector3 nv = new Vector3(vx * u, vy * u, vz * u);
+			Vector3 nv = ScaleVector(vx, vy, vz);
 			pool[key] = nv;
 			return nv;
 		}
